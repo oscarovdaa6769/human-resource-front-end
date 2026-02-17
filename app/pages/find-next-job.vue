4723767ad1a3b2260e1cc5a70b69e91da9fbf8b6
Neath <neath@gmail.com>
2026-02-17 08:39:26 +0700

    commit to pull code from main

--- app/pages/find-next-job.vue
+++ app/pages/find-next-job.vue
@@ -22,44 +22,58 @@
     </p>
 
 
-<div class="px-2 py-2 flex items-center border rounded-2xl overflow-hidden w-full max-w-2xl mx-auto">
-<select
-  class="px-4 py-3 rounded-sm border  text-[#ffffff] bg-secondary"
->
-  <option value="">Job little</option>
-  <option>Frontend</option>
-  <option>Backend</option>
-  <option>Design</option>
-  <option>Marketing</option>
-
-</select>
+<section class=" py-12 mx-auto">
+  <div class="max-w-6xl mx-auto px-4">
 
 
+    <div class="flex flex-col md:flex-row items-center gap-3  shadow-lg rounded-2xl p-3 bg-white/8">
 
+      <div class="flex items-center flex-1 border rounded-xl px-4 py-2">
+        <svg class="w-5 h-5 text-[#333333] mr-2" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
+          <path d="M21 21l-4.3-4.3m1.8-5.2a7 7 0 11-14 0 7 7 0 0114 0z"/>
+        </svg>
 
+        <input
+          type="text"
+          placeholder="Job title or keyword"
+          class="w-full outline-none bg-transparent"
+        />
+      </div>
 
+      <div class="flex-1 ">
+        <select class="w-full border rounded-xl px-4 py-2 outline-none ">
+          <option>All Locations</option>
+          <option>Phnom Penh</option>
+          <option>Siem Reap</option>
+          <option>Battambang</option>
+        </select>
+      </div>
 
-  <input 
-    type="select" 
-    placeholder="" 
-    class="flex-1 px-4 py-2 outline-none"
-  />
-  <button class="bg-primary text-[#333333] px-6 py-2 transition-all duration-300 ease-in-out hover:bg-accent rounded-xl ">
-    Search
-  </button>
-</div>
+      <div class="flex-1">
+        <select class="w-full border rounded-xl px-4 py-2 outline-none">
+          <option>All Categories</option>
+          <option>IT</option>
+          <option>Marketing</option>
+          <option>Finance</option>
+        </select>
+      </div>
 
+   
+      <button
+        class="bg-primary text-[#333333] px-8 py-3 rounded-xl hover:bg-secondary transition"
+      >
+        Search
+      </button>
 
-
-
-
-
-
-
- 
-
+    </div>
   </div>
 </section>
+
+
+</div>
+
+</section>
+
 
 
 </template>
