55dd4c7b21bb24123d89b07eabe31ee44ddd474e
nita <chheunnita169@gmail.com>
2026-02-17 15:28:51 +0700

    commit to pull code from main

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,26 +1,20 @@
 <template>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
   <section class="relative w-full min-h-[500px] bg-gray-900">
-    <!-- Background image -->
     <img
       src="https://i.pinimg.com/736x/77/6b/a1/776ba1584ae387d0891c5b7d5a64cde4.jpg"
       alt="Team working together"
       class="absolute inset-0 w-full h-full object-cover opacity-40"
     />
-
-    <!-- Content -->
     <div class="relative z-10 max-w-7xl mx-auto px-6 py-24 flex items-center">
       <div class="max-w-xl text-white">
-        <p class="text-sm uppercase tracking-wide text-blue-400 mb-3">
-          Experience the Best IT Solutions
-        </p>
+    
 
         <h1 class="text-4xl md:text-5xl font-bold leading-tight mb-6">
-          Where Creativity <br />
-          Meets Cutting-Edge <br />
-          Technology
+         Job opprtunties
         </h1>
 
-        <p class="text-gray-300 mb-8">
+        <p class="text-gray-300 mb-8 ">
           I build secure and intuitive HR web solutions that streamline
            recruitment, employee records, payroll, attendance, and performance 
            management. These solutions reduce manual work, improve data accuracy,
@@ -30,23 +24,166 @@
 
         <div class="flex gap-4">
           <button
-            class="bg-primary hover:bg- text-white px-6 py-3 rounded-full font-medium transition"
+            class="bg-primary hover:bg-accent text-[#000] hover:text-[#fff] px-6 py-3 rounded-full font-medium transition"
           >
             Explore More →
           </button>
 
-          <button
-            class="border-2 border-accent text-white px-6 py-3 rounded-full font-medium hover:bg-accent hover:text-white transition"
-          >
-            View All Services
-          </button>
+          
         </div>
+      </div>
+      
+    </div>
+    
+  </section>
+  <section class="min-h-screen flex items-center justify-center text-blue-900 px-4">
+    <div class="text-center max-w-3xl">
+      <h1 class="text-3xl sm:text-4xl  font-bold text-white mb-10">
+        Where startups and job seekers connect
+      </h1>
+      <div class="flex flex-col sm:flex-row items-center justify-center gap-4">
+        <button
+          class="bg-primary text-secondary px-8 py-4 rounded-full text-lg font-medium hover:bg-accent hover:text-white transition"
+        >
+          Find your next hire
+        </button>
+        <button
+          class="border-2 border-primary text-primary px-8 py-4 rounded-full text-lg font-medium hover:bg-accent transition hover:text-white"
+        >
+          Find your next job
+        </button>
       </div>
     </div>
   </section>
+  <section class="w-full bg-secondary flix ">
+    <div class="max-w-full  grid grid-cols-1 md:grid-cols-2">
+
+      <div class="px-8 md:px-16 bg-secondary">
+        <p class="text-2xl font-semibold text-white mb-3 pt-9">Got talent?</p>
+        <h2 class="text-4xl  text-white font-bold mb-12">
+          Why job seekers love us
+        </h2>
+
+        <div class="space-y-10">
+          <div class="flex gap-5">
+            <div class="w-15 h-12 flex items-center justify-center rounded-full bg-primary-100 text-accent">
+            <i class="fa-solid fa-arrow-trend-up"></i>
+
+            </div>
+            <p class="text-gray-700">
+              Connect directly with founders at top startups – no third party recruiters allowed.
+            </p>
+          </div>
+
+          <div class="flex gap-5">
+            <div class="w-17 h-12 flex items-center justify-center rounded-full bg-primary-100  text-accent">
+            <i class="fa-solid fa-book-open"></i>
+            </div>
+            <p class="text-gray-700">
+              Everything you need to know, all upfront. View salary, stock options, and more before applying.
+            </p>
+          </div>
+
+          <div class="flex gap-5">
+            <div class="w-17 h-12 flex items-center justify-center rounded-full bg-primary-100  text-accent">
+            <i class="fa-solid fa-arrows-down-to-people"></i>
+
+            </div>
+            <p class="text-gray-700">
+              Say goodbye to cover letters – your profile is all you need. One click to apply and you're done.
+            </p>
+          </div>
+
+          <div class="flex gap-5">
+            <div class="w-13 h-12 flex items-center justify-center rounded-full bg-primary-100 text-accent">
+            <i class="fa-solid fa-star"></i>
+
+            </div>
+            <p class="text-gray-700">
+              Unique jobs at startups and tech companies you can’t find anywhere else.
+            </p>
+          </div>
+        </div>
+
+        <div class="mt-12 flex gap-4 pb-8" >
+          <button class="px-6 py-3  border-2 border-primary hover:bg-accent hover:text-secondary rounded-lg font-medium ">
+            Learn more
+          </button>
+          <button class="px-6 py-3  bg-primary hover:bg-accent  text-[#000] hover:text-[#fff] rounded-lg font-medium">
+            Sign up
+          </button>
+        </div>
+      </div>
+
+
+    
+      <div class="px-8 md:px-16 bg-highlight">
+        <p class="text-2xl font-semibold text-white mb-3 pt-9">Got talent?</p>
+        <h2 class="text-4xl  text-white font-bold mb-12">
+          Why job seekers love us
+        </h2>
+
+        <div class="space-y-10">
+          <div class="flex gap-5">
+            <div class="w-15 h-12 flex items-center justify-center rounded-full bg-primary-100 text-accent">
+            <i class="fa-solid fa-arrow-trend-up"></i>
+
+            </div>
+            <p class="text-gray-700">
+              Connect directly with founders at top startups – no third party recruiters allowed.
+            </p>
+          </div>
+
+          <div class="flex gap-5">
+            <div class="w-17 h-12 flex items-center justify-center rounded-full bg-primary-100  text-accent">
+            <i class="fa-solid fa-book-open"></i>
+            </div>
+            <p class="text-gray-700">
+              Everything you need to know, all upfront. View salary, stock options, and more before applying.
+            </p>
+          </div>
+
+          <div class="flex gap-5">
+            <div class="w-17 h-12 flex items-center justify-center rounded-full bg-primary-100  text-accent">
+            <i class="fa-solid fa-arrows-down-to-people"></i>
+
+            </div>
+            <p class="text-gray-700">
+              Say goodbye to cover letters – your profile is all you need. One click to apply and you're done.
+            </p>
+          </div>
+
+          <div class="flex gap-5">
+            <div class="w-13 h-12 flex items-center justify-center rounded-full bg-primary-100 text-accent">
+            <i class="fa-solid fa-star"></i>
+
+            </div>
+            <p class="text-gray-700">
+              Unique jobs at startups and tech companies you can’t find anywhere else.
+            </p>
+          </div>
+        </div>
+
+        <div class="mt-12 flex gap-4 pb-8" >
+          <button class="px-6 py-3  border-2 border-primary hover:bg-accent hover:text-secondary rounded-lg font-medium ">
+            Learn more
+          </button>
+          <button class="px-6 py-3  bg-primary hover:bg-accent  text-[#000] hover:text-[#fff] rounded-lg font-medium">
+            Sign up
+          </button>
+        </div>
+      </div>
+
+      
+      
+
+    </div>
+  </section>
+
+  
 </template>
 
-<script setup>
-// No logic needed for static hero
+<script setup> 
+
 </script>
 
