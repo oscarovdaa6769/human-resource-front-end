dc3d0557e91e0a2621abdd9ddc6d750da4cef275
SAMI Sokbuntha <samisokbuntha@gmail.com>
2026-02-17 08:33:20 +0700

    commit for pull from main

---
+++ app/pages/companies-page.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="w-full banner">
+        <div class="container pt-10 pb-10 flex flex-col items-center gap-10">
+            <h2 class="text-primary text-7xl font-black text-center">Find the job made for you.</h2>
+            <p class=" text-center font-black">We make it easy to find what's next. </p>
+            <p class=" text-center font-black">Browse over 100,000 jobs — from top companies to fast-growing startups.</p>
+           
+
+        </div>
+    </div>
+</template>
